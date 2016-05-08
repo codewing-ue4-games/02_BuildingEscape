fc487bc783978c3c2da40733998f071e3d36cb13
codewing <codewing@web.de>
2016-05-08 12:04:38 +0200

    Position Reporter logs the position on startup

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -21,8 +21,9 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 }
 
 
