6e8945435720231f307c8ef7e8ef382b2beb6e84
codewing <codewing@web.de>
2016-05-09 18:48:09 +0200

    Added parts of the starter content Changed gitignore Updated grabber

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,8 @@
 #include "BuildingEscape.h"
 #include "Grabber.h"
 
+#define OUT
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -31,6 +33,23 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
-}
+	FVector PlayerLocation;
+	FRotator PlayerRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerLocation, 
+		OUT PlayerRotation
+	);
 
+	/*UE_LOG(LogTemp, Warning, TEXT("Pos: %s - Rot: %s"), *PlayerLocation.ToString(), *PlayerRotation.ToString());*/
+	FVector LineTraceEnd = PlayerLocation + PlayerRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		.0f,
+		0,
+		10.f);
+}
